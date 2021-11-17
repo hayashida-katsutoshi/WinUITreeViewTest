1e8942a16ed7481a4125d4da1697f4a8794868af
Harry <hk.13701@konamigaming.com>
2021-11-17 09:50:05 -0800

    Add project files.

---
+++ WinUITreeViewTest/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUITreeViewTest
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            ViewModel = new MainWindowViewModel();
+
+            ObservableCollection<MyItem> root = new ObservableCollection<MyItem>();
+
+            MyItem item1 = new MyItem() { Name = "Item1" };
+            root.Add(item1);
+            item1.Children.Add(new MyItem() { Name = "Item1.1" });
+            item1.Children.Add(new MyItem() { Name = "Item1.2" });
+            item1.Children.Add(new MyItem() { Name = "Item1.3" });
+
+            MyItem item2 = new MyItem() { Name = "Item2" };
+            root.Add(item2);
+            item1.Children.Add(new MyItem() { Name = "Item2.1" });
+            item1.Children.Add(new MyItem() { Name = "Item2.2" });
+            item1.Children.Add(new MyItem() { Name = "Item2.3" });
+
+            MyItem item3 = new MyItem() { Name = "Item3" };
+            root.Add(item3);
+            item1.Children.Add(new MyItem() { Name = "Item3.1" });
+            item1.Children.Add(new MyItem() { Name = "Item3.2" });
+            item1.Children.Add(new MyItem() { Name = "Item3.3" });
+
+            ViewModel.MyItems = root;
+
+            MyItemView.DataContext = ViewModel;
+        }
+
+        MainWindowViewModel ViewModel;
+    }
+}
---
+++ WinUITreeViewTest/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MvvmGen;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUITreeViewTest
+{
+    [ViewModel]
+    public partial class MainWindowViewModel
+    {
+        [Property]
+        ObservableCollection<MyItem> myItems;
+    }
+
+    [ViewModel]
+    public partial class MyItem
+    {
+        public string Name { get; set; }
+
+        public override string ToString() => Name;
+
+        [Property]
+        private bool? isSelected;
+
+        [Property]
+        private bool isExpanded;
+
+        [Property]
+        private ObservableCollection<MyItem> children = new ObservableCollection<MyItem>();
+    }
+}
